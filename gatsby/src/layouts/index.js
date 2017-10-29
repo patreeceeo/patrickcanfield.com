db048972bcbbdcb79840b04285fdc1aad8c8c95b
Patrick Canfield <pscale01@gmail.com>
2017-10-29 13:28:51 -0700

    add favicons, normalize.css

--- gatsby/src/layouts/index.js
+++ gatsby/src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
-import './index.css';
+import './normalize.css';
+import './typography.css';
 
 const Header = () => (
   <div
@@ -38,11 +39,22 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       titleTemplate="%s - Patrick Canfield"
+      defaultTitle="Patrick Canfield"
       meta={[
         { name: 'description', content: 'Personal site of Patrick Canfield' },
         { name: 'keywords', content: '' },
       ]}
-    />
+    >
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=YAo0PGOwK0"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=YAo0PGOwK0"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=YAo0PGOwK0"/>
+        <link rel="manifest" href="/manifest.json?v=YAo0PGOwK0"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=YAo0PGOwK0" color="#5bbad5"/>
+        <link rel="shortcut icon" href="/favicon.ico?v=YAo0PGOwK0"/>
+        <meta name="apple-mobile-web-app-title" content="It's me, Patricio"/>
+        <meta name="application-name" content="It's me, Patricio"/>
+        <meta name="theme-color" content="#ffffff"/>
+    </Helmet>
     <Header />
     <div
       style={{
